c18383f05aecfc77b3761081cbfb26573852d284
suhankins <punishedbernadetta@niuitmo.ru>
2022-12-24 22:11:42 +0400

    removed debug duck

--- src/main.ts
+++ src/main.ts
@@ -6,30 +6,44 @@ import LoadAssets from './LoadAssets';
 import IsometricCamera from './IsometricCamera';
 import Lake from './Entities/Lake';
 import WindowToWorld from './WindowToWorld';
-import Entity from './Entities/Entity';
+import { Entity } from './Entities/Entity';
 import Bread from './Entities/Bread';
 
 // Checking if WebGL is supported
 if (WebGL.isWebGLAvailable()) {
     init();
 } else {
-	const warning = WebGL.getWebGLErrorMessage();
-	document.body.appendChild(warning);
+    const warning = WebGL.getWebGLErrorMessage();
+    document.body.appendChild(warning);
 }
 
+/**
+ * Used for delta time calculation
+ */
 const clock: Clock = new Clock();
+/**
+ * Used for seeing where did the user click in the game world
+ */
 const raycaster: Raycaster = new Raycaster();
 
 var renderer: WebGLRenderer;
 var scene: Scene;
 var camera: IsometricCamera;
 
+/**
+ * Lake itself. Should be stored here to be called on window resize.
+ */
 var lake: Lake;
 
-var debugDuck: Duck;
-
+/**
+ * List of all entities that should be updated on the scene.
+ */
 const entities: Entity[] = [];
 
+/**
+ * Adds an entity to the scene and to the list of entities to be updated.
+ * @param entity
+ */
 function addEntity(entity: Entity) {
     entities.push(entity);
     scene.add(entity.model);
@@ -45,18 +59,18 @@ async function init() {
 
     scene = new Scene();
     camera = new IsometricCamera();
-    //scene.add(new AmbientLight(0xffffff, 1.0));
 
     lake = new Lake();
     scene.add(lake.model);
 
     for (let i = 0; i < 1; i++) {
         const duck = new Duck();
-        debugDuck = duck;
-        duck.model.position.copy(WindowToWorld(
-            Math.random() * window.innerWidth,
-            Math.random() * window.innerHeight
-        ));
+        duck.model.position.copy(
+            WindowToWorld(
+                Math.random() * window.innerWidth,
+                Math.random() * window.innerHeight
+            )
+        );
         duck.model.rotation.set(0, Math.random() * Math.PI * 2, 0);
         addEntity(duck);
     }
@@ -73,19 +87,23 @@ function animate() {
         entity.update(dt);
     });
 
-	renderer.render(scene, camera);
+    renderer.render(scene, camera);
 }
 
+/**
+ * Bread spawner
+ */
 addEventListener('mousedown', (event) => {
-    raycaster.setFromCamera({
-        x: (event.clientX / window.innerWidth) * 2 - 1,
-        y: -(event.clientY / window.innerHeight) * 2 + 1
-    }, camera);
+    raycaster.setFromCamera(
+        {
+            x: (event.clientX / window.innerWidth) * 2 - 1,
+            y: -(event.clientY / window.innerHeight) * 2 + 1,
+        },
+        camera
+    );
 
     const intersect = raycaster.intersectObject(lake.model)[0].point;
-    intersect.y = 2; // So bread falls for a bit
-
-    debugDuck.lookAt(intersect);
+    intersect.y = 2 + Math.random(); // So bread falls for a bit
 
     const bread = new Bread(intersect);
     addEntity(bread);
