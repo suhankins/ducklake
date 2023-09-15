0902cf616cee613ba06e614060038b2d88d91861
suhankins <punishedbernadetta@niuitmo.ru>
2023-09-15 11:54:22 +0400

    made LoadAssets properly async

--- src/lib/utils/LoadAssets.ts
+++ src/lib/utils/LoadAssets.ts
@@ -1,10 +1,9 @@
 import Bread from '../entities/Bread';
 import Duck from '../entities/Duck/Duck';
 
-export default function LoadAssets(): Promise<void> {
-    return new Promise<void>(async (resolve) => {
-        await Duck.loadModel('./Duck.gltf');
-        await Bread.loadModel('./Bread.gltf');
-        resolve();
-    });
+export default function LoadAssets() {
+    return Promise.all([
+        Duck.loadModel('./Duck.gltf'),
+        Bread.loadModel('./Bread.gltf')
+    ])
 }
