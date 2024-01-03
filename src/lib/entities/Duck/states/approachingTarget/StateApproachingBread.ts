ede31040bfd6b7e5c1de7d6141c86d153c8d2176
suhankins <punishedbernadetta@niuitmo.ru>
2024-01-04 02:59:08 +0400

    added searching for closest bread when eager

--- src/lib/entities/Duck/states/approachingTarget/StateApproachingBread.ts
+++ src/lib/entities/Duck/states/approachingTarget/StateApproachingBread.ts
@@ -1,7 +1,25 @@
+import Bread from '../../../Bread';
 import { StateApproachingTarget } from './StateApproachingTarget';
 
+/**
+ * Duck moves towards target bread
+ * 
+ * Duck enters next state when:
+ * * Beak has touched the bread
+ * * Bread is gone
+ * * Duck is very hungry and duck isn't eager to reach given target
+ */
 export default class StateApproachingBread extends StateApproachingTarget {
     name: string = 'approaching target: bread';
+
+    update(dt: number) {
+        if (this.isEager) this.updateTarget();
+        super.update(dt);
+    }
+
+    updateTarget() {
+        this.target = Bread.getClosestBreadToPosition(this.position);
+    }
 
     /**
      * @override
