1bb20f42fccf4cc4a9fae2df020ec49e50a9d6b7
suhankins <punishedbernadetta@niuitmo.ru>
2024-02-17 16:04:53 +0300

    made LoadAssets async

--- src/lib/utils/LoadAssets.ts
+++ src/lib/utils/LoadAssets.ts
@@ -1,8 +1,8 @@
 import Bread from '../entities/Bread';
 import Duck from '../entities/Duck/Duck';
 
-export default function LoadAssets() {
-    return Promise.all([
+export default async function LoadAssets() {
+    return await Promise.all([
         Duck.loadModel('./Duck.gltf'),
         Bread.loadModel('./Bread.gltf'),
     ]);
