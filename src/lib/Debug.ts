215363d33d28865d3bd56da2eaabf185fbb5edb6
suhankins <punishedbernadetta@niuitmo.ru>
2024-03-01 21:41:48 +0300

    added debug function to spawn 10 ducks

--- src/lib/Debug.ts
+++ src/lib/Debug.ts
@@ -1,13 +1,20 @@
 import Duck from './entities/Duck/Duck';
 
+import type Game from './Game';
+
 export default class Debug {
-    constructor() {
+    constructor(game: Game) {
         document
             .getElementById('debug-make-ducks-hungry')
             ?.addEventListener('click', () => {
                 Object.values(Duck.ducks).forEach(
                     (duck) => (duck.hunger = -999)
                 );
+            });
+        document
+            .getElementById('debug-spawn-ten-ducks')
+            ?.addEventListener('click', () => {
+                for (let i = 0; i < 10; i++) game.addEntity(new Duck(game));
             });
     }
 }
--- src/main.ts
+++ src/main.ts
@@ -5,8 +5,8 @@ import WebGL from 'three/examples/jsm/capabilities/WebGL';
 
 // Checking if WebGL is supported
 if (WebGL.isWebGLAvailable()) {
-    new Game();
-    new Debug();
+    const game = new Game();
+    new Debug(game);
 } else {
     const warning = WebGL.getWebGLErrorMessage();
     document.body.appendChild(warning);
