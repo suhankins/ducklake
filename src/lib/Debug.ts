9a0119ac6418d6b04127e3f9014081fea4da2f8f
suhankins <punishedbernadetta@niuitmo.ru>
2024-03-12 21:33:17 +0400

    better organized Debug class

--- src/lib/Debug.ts
+++ src/lib/Debug.ts
@@ -3,7 +3,16 @@ import Duck from './entities/Duck/Duck';
 import type Game from './Game';
 
 export default class Debug {
+    game: Game;
+
     constructor(game: Game) {
+        this.game = game;
+        this.setupMakeDucksHungry();
+        this.setupSpawnManyDucks();
+        this.setupToggleDebugText();
+    }
+
+    setupMakeDucksHungry() {
         document
             .getElementById('debug-make-ducks-hungry')
             ?.addEventListener('click', () => {
@@ -11,16 +20,23 @@ export default class Debug {
                     (duck) => (duck.hunger = -999)
                 );
             });
+    }
+
+    setupSpawnManyDucks() {
         document
             .getElementById('debug-spawn-ten-ducks')
             ?.addEventListener('click', () => {
-                for (let i = 0; i < 10; i++) game.addEntity(new Duck(game));
+                for (let i = 0; i < 10; i++)
+                    this.game.addEntity(new Duck(this.game));
             });
+    }
+
+    setupToggleDebugText() {
         document
             .getElementById('debug-text')
             ?.addEventListener('change', (event) => {
                 const target = event.target as HTMLInputElement;
-                game.debugTextEnabled = target.checked;
+                this.game.debugTextEnabled = target.checked;
             });
     }
 }
