7a4d78d5d0ff2efec87c5f2295833f83061a7710
suhankins <punishedbernadetta@niuitmo.ru>
2024-04-02 21:36:56 +0400

    made ducklake a PWA

--- src/main.ts
+++ src/main.ts
@@ -2,11 +2,19 @@ import Debug from './lib/Debug';
 import Game from './lib/Game';
 import './style.scss';
 import WebGL from 'three/examples/jsm/capabilities/WebGL';
+import { registerSW } from "virtual:pwa-register";
 
 // Checking if WebGL is supported
 if (WebGL.isWebGLAvailable()) {
     const game = new Game();
     new Debug(game);
+    const updateSW = registerSW({
+        onNeedRefresh() {
+            if (confirm('New content available. Reload?')) {
+                updateSW(true);
+            }
+        },
+    });
 } else {
     const warning = WebGL.getWebGLErrorMessage();
     document.body.appendChild(warning);
